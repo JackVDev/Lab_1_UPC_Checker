c85aeaa697816c96dec4435a8035e298f66afc64
Jack Verdin <jackverdin@icloud.com>
2026-06-19 14:40:03 -0400

    Initial commit

---
+++ UPC_Validator.py
@@ -0,0 +1,10 @@
+"""
+UPC_Valiator.py
+By Jack Verdin
+This program was made for a CSCC Python course, and is meant to validate a UPC-A code.
+"""
+
+#Need find_UPC() function
+
+#Need input logic
+#Need output logic
