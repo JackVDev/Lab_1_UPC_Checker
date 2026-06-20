1d65490ed134c3e2881f29c24d546096ff5fa4e5
Jack Verdin <jackverdin@icloud.com>
2026-06-20 10:37:15 -0400

    Added input logic

--- UPC_Validator.py
+++ UPC_Validator.py
@@ -7,4 +7,14 @@ This program was made for a CSCC Python course, and is meant to validate a UPC-A
 #Need find_UPC() function
 
 #Need input logic
+upc_code = ""
+while len(upc_code) != 12:
+    upc_code = input("Enter a UPC code: ")
+    if len(upc_code) != 12:
+        print("That is not the valid length for a UPC code.")
+        upc_code = ""
+    elif not(upc_code.isdigit()):
+        print("UPC codes cannot contain any letters.")
+        upc_code = ""
+print("Loop over")
 #Need output logic
